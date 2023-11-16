716069dd20247e172da2705500a5e7684d601020
grubnev <grubnev@outlook.com>
2023-11-16 18:08:29 +0500

    add files

--- practice_3/Task_2/Task_2.py
+++ practice_3/Task_2/Task_2.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import numpy as np
 import re
 import json
 
@@ -40,11 +41,31 @@ for product in items:
     if product['price'] > 100000:
         filtered_items.append(product)
 
-print(len(items))
-print(len(filtered_items))
+print("Колличество всех продуктов", len(items))
+print("Колличество отсортированных продуктов", len(filtered_items))
 
 with open("result_filtred_1.json", "w", encoding="utf-8") as f:
     f.write(json.dumps(filtered_items))
 
 with open("result_all_1.json", "w", encoding="utf-8") as f:
     f.write(json.dumps(items))
+
+price_array = np.array([items['price'] for item in items])
+
+print("Максимальное значение Price:", np.max(price_array))
+print("Минимальное значение Price:", np.min(price_array))
+print("Сумма значений Price:", np.sum(price_array))
+print("Среднее арифметическое значений Price:", np.mean(price_array))
+
+title_array = np.array([items['title'] for item in items])
+
+word_frequency = {}
+for word in title_array:
+    if word in word_frequency:
+        word_frequency[word] += 1
+    else:
+        word_frequency[word] = 1
+
+sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_word_frequency)
