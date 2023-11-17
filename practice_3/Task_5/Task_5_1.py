4895a7c7614e179f1edab1023c5a0ae119d5be26
grubnev <grubnev@outlook.com>
2023-11-17 15:03:23 +0500

    add files

--- practice_3/Task_5/Task_5_1.py
+++ practice_3/Task_5/Task_5_1.py
@@ -3,9 +3,9 @@ import numpy as np
 import re
 import json
 
-
 def handle_file(file_name):
-    with open(file_name, encoding='utf-8') as file:
+
+    with open(file_name, encoding="utf-8") as file:
         text = ""
         for row in file.readlines():
             text += row
@@ -13,82 +13,16 @@ def handle_file(file_name):
         site = BeautifulSoup(text, 'html.parser')
 
         item = dict()
+        item['title'] = site.find_all('h1')[0].get_text()
+        item['price'] = int(site.find_all('span', attrs={"itemprop": "price"})[0].get_text())
 
-        item['title'] = site.find_all("h1", attrs={"itemprop": "name"})[0].get_text()
-        item['price'] = int(site.find_all("span", attrs={"itemprop": "price"})[0].get_text())
+        props = site.find_all('div', attrs={"class":"col"})
+        for prop in props:
+            item[prop['type']] = prop.get_text().strip()
 
-        rows = site.find_all('div', class_='row')
-        for row in rows:
-            key = row.find('div', class_='col').text.strip()
-            value = row.find('div', class_='col').find_next('div', class_='col').text.strip()
         items.append(item)
-        item[key] = value
 
-
-        # item['city'] = site.find_all("span", string=re.compile("Город:"))[0].get_text().replace("Город:", "").strip()
-        # item['build'] = site.find_all("h1")[0].get_text().replace("Строение:","").strip()
-        # address = site.find_all("p", attrs={"class": "address-p"})[0].get_text().split("Индекс:")
-        # item['address'] = address[0].replace("Улица:","").strip()
-        # item['index'] = address[1].strip()
-        # item['floors'] = int(site.find_all("span", attrs={"class": "floors"})[0].get_text().split(":")[1].strip())
-        # item['year'] = int(site.find_all("span", attrs={"class": "year"})[0].get_text().split(":")[0].replace("Построено в", "").strip())
-        # item['parking'] = site.find_all("span", string=re.compile("Парковка:"))[0].get_text().replace("Парковка:", "").strip()
-        # item['parking'] = item["parking"] == 'есть'
-        # item['img'] = site.find_all("img")[0]['src']
-        # item['rating'] = float(site.find_all("span", string=re.compile("Рейтинг:"))[0].get_text().split(":")[1].strip())
-        # item['views'] = int(site.find_all("span", string=re.compile("Просмотры:"))[0].get_text().split(":")[1].strip())
-        print(item['title'])
-        print(item['price'])
         print(item)
-        return item
 
 items = []
-
-handle_file("1/1.html")
-
-
-# for i in range(1,1000):
-#     file_name = f"1/{i}.html"
-#     result = handle_file(file_name)
-#     items.append(result)
-
-# #Сортировка по views
-#
-# items = sorted(items, key=lambda x: x['views'], reverse = True)
-#
-# with open("result_all_1.json", "w", encoding="utf-8") as f:
-#     f.write(json.dumps(items))
-#
-# #Определяем количество строений рейтинг которых больше 2
-#
-# filtered_items = []
-# for building in items:
-#     if building['rating'] >= 2:
-#         filtered_items.append((building))
-#
-# print("Всего элементов:", len(items))
-# print("Фильтрация элементов:", len(filtered_items))
-#
-# #Статистические характеристики для views
-#
-# views_array = np.array([item['views'] for item in items])
-#
-# print("Максимальное значение Views:", np.max(views_array))
-# print("Минимальное значение Views:", np.min(views_array))
-# print("Сумма значений Views:", np.sum(views_array))
-# print("Среднее арифметическое значений Views:", np.mean(views_array))
-#
-# #Частота меток city
-#
-# city_array = np.array([item['city'] for item in items])
-#
-# word_frequency = {}
-# for word in city_array:
-#     if word in word_frequency:
-#         word_frequency[word] += 1
-#     else:
-#         word_frequency[word] = 1
-#
-# sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
-#
-# print(sorted_word_frequency)
+handle_file('1/1.html')
---
+++ practice_3/Task_5/Task_5_2.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import numpy as np
+import re
+import json
+
+def handle_file(file_name):
+
+    with open(file_name, encoding="utf-8") as file:
+        text = ""
+        for row in file.readlines():
+            text += row
+
+        site = BeautifulSoup(text, 'html.parser')
+        products = site.find_all("div", attrs={'class': 'block visible'})
+
+        item = dict()
+        for product in products:
+            item['title'] = product.find_all("a")[0].get_text().strip()
+            item['price'] = int(product.find_all("div", attrs={'class': 'price'})[0].get_text().replace(" Р.", "").strip())
+            item['delivery'] = product.find_all("div", attrs={'class': 'delivery'})[0].get_text().replace('<span class="icon-truck"></span>', "").strip()
+        items.append(item)
+    return item
+
+items = []
+for i in range(1, 6):
+    file_name = f"2/{i}.html"
+    result = handle_file(file_name)
+    items.append(result)
+del items[-1]
+del items[-1]
+print(items)
+#Сортировка по price
+
+items = sorted(items, key=lambda x: x['price'], reverse=True)
+
+#Определяем количество продуктов стоимость которых больше 100 000р
+filtered_items = []
+for product in items:
+    if product['price'] > 500:
+        filtered_items.append(product)
+
+print("Колличество всех продуктов", len(items))
+print("Колличество отсортированных продуктов", len(filtered_items))
+
+with open("result_filtred_5.2.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(filtered_items))
+
+with open("result_all_5.2.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(items))
+
+#Статистические характеристики для radius
+
+price_array = np.array([item['price'] for item in items])
+
+print("Максимальное значение Price:", np.max(price_array))
+print("Минимальное значение Price:", np.min(price_array))
+print("Сумма значений Price:", np.sum(price_array))
+print("Среднее арифметическое значений Price:", np.mean(price_array))
+
+#Частота меток title
+
+title_array = np.array([item['title'] for item in items])
+
+word_frequency = {}
+for word in title_array:
+    if word in word_frequency:
+        word_frequency[word] += 1
+    else:
+        word_frequency[word] = 1
+
+sorted_word_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_word_frequency)
