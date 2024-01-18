ed8d9b605d363cb4c2d0827c2623a9e71248c670
grubnev <grubnev@outlook.com>
2024-01-18 17:59:59 +0500

    practice 6

--- practice_6/main.py
+++ practice_6/main.py
@@ -15,37 +15,91 @@ def get_memory_stat_by_column(df):
     memory_usage_stat = df.memory_usage(deep=True)
     # сколько всего места занимает файл
     total_memory_usage = memory_usage_stat.sum()
-    # print(f"file size       ={file_size // 1024:10} кб")
-    # print(f"file in memory size ={total_memory_usage // 1024:10} кб")
+    print(f"file size       ={file_size // 1024:10} кб")
+    print(f"file in memory size ={total_memory_usage // 1024:10} кб")
     column_stat = list()
-    for key in dataset.dtypes.keys():
+    for key in df.dtypes.keys():
         column_stat.append({
             "column_name": key,
             "memory_abs": memory_usage_stat[key] // 1024,
             "memory_per": round(memory_usage_stat[key] / total_memory_usage * 100, 4),
-            "dtype": str(dataset.dtypes[key])
+            "dtype": str(df.dtypes[key])
         })
         # вычислить для каждой колонки занимаемый объем памяти, долю от общего объема, а также выяснить тип данных
     column_stat.sort(key=lambda x: x['memory_abs'], reverse=True)
     for column in column_stat:
-     print(f"{column['column_name']:30}: {column['memory_abs']:10} кб: {column['memory_per']:10}%: {column['dtype']}")
+        print(f"{column['column_name']:30}: {column['memory_abs']:10} кб: {column['memory_per']:10}%: {column['dtype']}")
 
-    return column_stat
+def mem_usage(pandas_obj):
+    if isinstance(pandas_obj, pd.DataFrame):
+        usage_b = pandas_obj.memory_usage(deep=True).sum()
+    else: # если не df а серия
+        usage_b = pandas_obj.memory_usage(deep=True)
+    usage_mb = usage_b/1024**2
+    return "{:03.2f} MB".format(usage_mb)
+
+def opt_obj(df):
+    converted_obj=pd.DataFrame()
+    dataset_obj=df.select_dtypes(include=['object']).copy() # все колонки по типам
+    print(dataset_obj.describe())
+# Преобразовать все колонки с типом данных «object» в категориальные,
+# если количество уникальных значений колонки составляет менее 50%.
+    for col in dataset_obj.columns:
+        num_unique_values=len(dataset_obj[col].unique()) # кол-во уникальных знач
+        num_total_values=len(dataset_obj[col]) # всего
+        if num_unique_values / num_total_values < 0.5: # если отношение меньше 50%, вводим категориальное поле
+            converted_obj.loc[:, col] = dataset_obj[col].astype('category')
+        else:
+            converted_obj.loc[:, col] = dataset_obj[col]
+
+    print(mem_usage(dataset_obj))
+    print(mem_usage((converted_obj)))
+    return converted_obj
+
+# Понижающее преобразование
+def opt_int(df):
+    dataset_int = df.select_dtypes(include=['int'])
+    """
+    downcast:
+            - 'integer' or 'signed': smallest signed int dtype (min.:np.int8)
+            - 'unsigned': smallest unsigned int dtype (min.: np.uint8)
+            - 'float': smallest float dtype (min.: np.float32)
+    """
+    converted_int = dataset_int.apply(pd.to_numeric, downcast='unsigned') # оптимизировать формат
+    print(mem_usage(dataset_int))
+    print(mem_usage(converted_int))
+# как изменились типы
+    compare_ints=pd.concat([dataset_int.dtypes, converted_int.dtypes], axis=1)
+    compare_ints.columns=['before', 'after']
+    compare_ints.apply(pd.Series.value_counts)
+    print(compare_ints)
+    return converted_int
+
+def opt_float(df):
+    dataset_float = df.select_dtypes(include=['float'])
+    converted_float = dataset_float.apply(pd.to_numeric, downcast='float')
+
+    print(mem_usage(dataset_float))
+    print(mem_usage(converted_float))
+
+    compare_floats = pd.concat([dataset_float.dtypes, converted_float.dtypes], axis=1)
+    compare_floats.columns=['before', 'after']
+    compare_floats.apply(pd.Series.value_counts)
+    print(compare_floats)
+    return converted_float
 
 # 1 датасет
 #file_name="data\[1]game_logs.csv"
-
 # 2 датасет
-file_name="data\[2]automotive.csv\CIS_Automotive_Kaggle_Sample.csv"
-
+#file_name="data\[2]automotive.csv\CIS_Automotive_Kaggle_Sample.csv"
 #3 датасет
 # file_name="data\[3]flights.csv"
 
 # read dataset
-dataset=read_file(file_name)
+df=read_file(file_name)
 file_size=os.path.getsize(file_name)
-get_memory_stat_by_column(dataset)
-column_stat = get_memory_stat_by_column(dataset)
+get_memory_stat_by_column(df)
+column_stat = get_memory_stat_by_column(df)
 
 # Сортировка
 column_stat_sorted = sorted(column_stat, key=lambda x: x['memory_abs'], reverse=True)
@@ -59,85 +113,32 @@ with open(output_file, "w", encoding="utf-8") as json_file:
     json.dump(column_stat_sorted, json_file, ensure_ascii=False)
 
 for dtype in ['float', 'int', 'object']:
-    selected_dtype=dataset.select_dtypes(include=[dtype])
+    selected_dtype=df.select_dtypes(include=[dtype])
     mean_usage_b=selected_dtype.memory_usage(deep=True).mean()
     mean_usage_mb=mean_usage_b/ 1024**2
     print("Использование памяти в среднем для {} столбцов: {:03.2f} MB".
           format(dtype, mean_usage_mb))
 
-def mem_usage(pandas_obj):
-    if isinstance(pandas_obj, pd.DataFrame):
-        usage_b=pandas_obj.memory_usage(deep=True).sum()
-    else: # если не df а серия
-        usage_b=pandas_obj.memory_usage(deep=True)
-    usage_mb=usage_b/1024**2
-    return "{:03.2f} MB".format(usage_mb)
+
 
 converted_obj=pd.DataFrame()
 #dow=dataset.day_of_week
 #mem_usage(dow)
 #opt_dow=dow.astype('category')
-print(mem_usage(dataset))
+print(mem_usage(df))
 
-def opt_obj(df):
-    convert_obj=pd.DataFrame
-    dataset_obj=dataset.select_dtypes(include=['object']).copy() # все колонки по типам
-    print(dataset_obj.describe())
-# Преобразовать все колонки с типом данных «object» в категориальные,
-# если количество уникальных значений колонки составляет менее 50%.
-    for col in dataset_obj.columns:
-        num_unique_values=len(dataset_obj[col].unique()) # кол-во уникальных знач
-        num_total_values=len(dataset_obj[col]) # всего
-        if num_unique_values/num_total_values <0.5: # если отношение меньше 50%, вводим категориальное поле
-            converted_obj.loc[: , col]=dataset_obj[col].astype('category')
-        else:
-            converted_obj.loc[:, col]=dataset_obj[col]
 
-    print(mem_usage(dataset_obj))
-    print(mem_usage((converted_obj)))
-    return converted_obj
 
-# Понижающее преобразование
-def opt_int(df):
-    dataset_int=dataset.select_dtypes(include=['int'])
-    """
-    downcast:
-            - 'integer' or 'signed': smallest signed int dtype (min.:np.int8)
-            - 'unsigned': smallest unsigned int dtype (min.: np.uint8)
-            - 'float': smallest float dtype (min.: np.float32)
-    """
-    converted_int=dataset_int.apply(pd.to_numeric, downcast='unsigned') # оптимизировать формат
-    print(mem_usage(dataset_int))
-    print(mem_usage(converted_int))
-# как изменились типы
-    compare_ints=pd.concat([dataset_int.dtypes, converted_int.dtypes], axis=1)
-    compare_ints.columns=['before', 'after']
-    compare_ints.apply(pd.Series.value_counts)
-    print(compare_ints)
-    return converted_int
-
-def opt_float(df):
-    dataset_float=dataset.select_dtypes(include=['float'])
-    converted_float=dataset_float.apply(pd.to_numeric, downcast='float')
-    print(mem_usage(dataset_float))
-    print(mem_usage(converted_float))
-
-    compare_floats=pd.concat([dataset_float.dtypes, converted_float.dtypes], axis=1)
-    compare_floats.columns=['before', 'after']
-    compare_floats.apply(pd.Series.value_counts)
-    print(compare_floats)
-    return converted_float
-
-optimized_dataset=dataset.copy()
-converted_int=opt_int(dataset)
-converted_obj=opt_obj(dataset)
-converted_float=opt_float(dataset)
+optimized_dataset=df.copy()
+converted_int=opt_int(df)
+converted_obj=opt_obj(df)
+converted_float=opt_float(df)
 
 optimized_dataset[converted_int.columns]=converted_int
 optimized_dataset[converted_float.columns]=converted_float
 optimized_dataset[converted_obj.columns]=converted_obj
 
-print(mem_usage(dataset))
+print(mem_usage(df))
 print(mem_usage(optimized_dataset))
 
 # Использование памяти
